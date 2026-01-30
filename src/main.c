3bed7ef3629f79ceb84fd5f0596dac97a68c407d
youngjoon <powerball701@gmail.com>
2026-01-29 23:36:26 -0800

    fixed new line issue

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
@@ -11,6 +12,8 @@ int main(int argc, char *argv[]) {
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
   fgets(command, sizeof(command), stdin);
+
+  command[strcspn(command, "\n")] = '\0';
 
   bool validCommand = false;
   if (validCommand) {}
