f03eaf6b39ee8f17788e46da3fb913c7b3d40c15
youngjoon <powerball701@gmail.com>
2026-01-29 23:45:36 -0800

    run infinite loop

--- src/main.c
+++ src/main.c
@@ -10,15 +10,16 @@ int main(int argc, char *argv[]) {
   char command[1024];
 
   // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
-  fgets(command, sizeof(command), stdin);
+  while (true) {
+    printf("$ ");
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0';
 
-  command[strcspn(command, "\n")] = '\0';
-
-  bool validCommand = false;
-  if (validCommand) {}
-  else {
-    printf("%s: command not found\n", command);
+    bool validCommand = false;
+    if (validCommand) {}
+    else {
+      printf("%s: command not found\n", command);
+    }
   }
 
   return 0;
