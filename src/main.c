a53e8378abfb9ef2c5a0eb7fc2045545593373d6
youngjoon <powerball701@gmail.com>
2026-01-29 23:33:41 -0800

    print command not found

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  char command[1024];
+
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+  fgets(command, sizeof(command), stdin);
+
+  bool validCommand = false;
+  if (validCommand) {}
+  else {
+    printf("%s: command not found\n", command);
+  }
 
   return 0;
 }
