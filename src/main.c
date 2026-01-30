6df3d0e9b038e0965177c89e2ab60ce8b687b080
youngjoon <powerball701@gmail.com>
2026-01-30 00:25:09 -0800

    implement echo

--- src/main.c
+++ src/main.c
@@ -3,27 +3,45 @@
 #include <stdbool.h>
 #include <string.h>
 
+void executeCommand(char* input) {
+  // extract command
+  char command[1024];
+  int i = 0;
+  while (*(input + i) != '\0' && *(input + i) != ' ') {
+    command[i] = *(input + i);
+    i++;
+  }
+  command[i] = '\0';
+
+  if (strcmp(command, "echo") == 0) {
+    printf("%s\n", input + i + 1);
+  } else {
+    printf("%s: command not found\n", command);
+  }
+}
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  char command[1024];
+  char input[1024];
 
   // TODO: Uncomment the code below to pass the first stage
   while (true) {
     printf("$ ");
-    fgets(command, sizeof(command), stdin);
-    command[strcspn(command, "\n")] = '\0';
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = '\0';
 
-    bool validCommand = false;
-    if (strcmp(command, "exit") == 0) {
+    if (strcmp(input, "exit") == 0) {
       break;
     }
-    if (validCommand) {}
-    else {
-      printf("%s: command not found\n", command);
-    }
+    
+    executeCommand(input);
   }
 
   return 0;
 }
+
+// char* extractCommand() {
+
+// }
