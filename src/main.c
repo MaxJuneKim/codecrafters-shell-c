3631c67af96cb234fe46dd023b23539efd2ec1c7
Youngjoon Kim <powerball701@gmail.com>
2026-03-12 17:55:08 -0700

    implemented locating executables with type

--- src/main.c
+++ src/main.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
+
+#if defined(__WIN32) || defined(__WIN64)
+  #include <windows.h>
+#endif
 
 #define total_commands 3
 
@@ -9,10 +14,9 @@ const char* const commands[total_commands] = {"echo", "type", "exit"};
 
 void executeType(char* argCommand) {
   for (int i = 0; i < total_commands; i++) { // For each builtin shell,
-    
     // compare argument command to the current builtin shell
     int k = 0;
-    while (argCommand[k] != '\0' && argCommand[k] != ' ' && commands[i][k] != '\0') {
+    while (argCommand[k] != '\0' && commands[i][k] != '\0') {
       if (argCommand[k] != commands[i][k]) {
         break;
       }
@@ -20,16 +24,47 @@ void executeType(char* argCommand) {
     }
 
     // if match, print "shell builtin" and return
-    if ((argCommand[k] == '\0' || argCommand[k] == ' ') && commands[i][k] == '\0') { 
-      char temp = argCommand[k];
-      argCommand[k] = '\0'; // dangerous?
+    if (argCommand[k] == '\0' && commands[i][k] == '\0') { 
       printf("%s is a shell builtin\n", argCommand);
-      argCommand[k] = temp;
       return;
     } 
   }
 
-  // if not found, print "not found"
+  // if not found, go through every path in PATH
+  // getting PATH variable
+  #if defined(__WIN32) || defined(__WINT64)
+    char* path_value = GetEnvironmentVariable("PATH");
+  #elif defined(__linux__) || defined(__unix__) || defined(TARGET_OS_MAC)
+    char* path_value = getenv("PATH");
+  #endif
+
+  char all_paths[strlen(path_value) + 1];
+  strcpy(all_paths, path_value);
+
+  // testing each path
+  #if defined(__WIN32)
+    char delimiter[2] = ";"
+  #elif defined(__linux__) || defined(__unix__) || defined(TARGET_OS_MAC)
+    char delimiter[2] = ":";
+  #endif
+
+  char* cur_path = strtok(all_paths, delimiter);
+  char cur_full_path[1024];
+  while (cur_path != NULL) {
+    // check if the file exists and permission is executable
+    strcpy(cur_full_path, cur_path);
+    strcat(cur_full_path, "/");
+    strcat(cur_full_path, argCommand);
+    if (access(cur_full_path, X_OK) == 0) { 
+      printf("%s is %s\n", argCommand, cur_full_path);
+      // printf("full path is: %s\n", cur_full_path);
+      return;
+    } 
+    // printf("full path is: %s\n", cur_full_path);
+    cur_path = strtok(NULL, delimiter);
+  }
+
+  // if not, print "not found"
   printf("%s: not found\n", argCommand);
 }
 
@@ -58,7 +93,6 @@ int main(int argc, char *argv[]) {
 
   char input[1024];
 
-  // TODO: Uncomment the code below to pass the first stage
   while (true) {
     printf("$ ");
     fgets(input, sizeof(input), stdin);
@@ -73,7 +107,3 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
-
-// char* extractCommand() {
-
-// }
